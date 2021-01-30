5f83596205532e4699be5e23e826a4f5e392280d
Niklas <niklassch21@gmail.com>
2021-01-30 17:29:39 +0100

    dynamic sizing

--- gui.v
+++ gui.v
@@ -4,6 +4,7 @@ import ui
 import gg
 import gx
 import time
+import sokol.sapp
 
 const (
 	col		=10
@@ -11,21 +12,28 @@ const (
 	//simulations per second
 	sps		=4
 	margin_top_to_grid=36
-	size		=30
 	padding		=4
-	grid_padding	=8
+	grid_padding	=12
 	dead		=gx.white
 	life		=gx.black
 	bg_win		=gx.rgb(200, 200, 200)
 	bg_grid		=gx.rgb(120, 120, 120)
-	win_width  = row*(padding+size)-padding+2*grid_padding
-	win_height = col*(padding+size)-padding+2*grid_padding+margin_top_to_grid
+	win_width  = 700
+	win_height = 396
+	//btn_map_width = 80
+	//btn_start_width = 80
+	//btn_row_width = 110
+	//btn_col_width = 110
 )
 
 struct App {
 mut:
 	window &ui.Window = 0
 	start	bool
+	size	int =30
+	margin_top int
+	margin_left int
+	
 	map	Map={
 		pattern:create_map(row, col, 0)
 		width:row
@@ -34,8 +42,8 @@ mut:
 }
 
 fn main() {
-	mut app := &App{
-	}
+	
+	mut app := &App{}
 	window := ui.window({
 		width: win_width
 		height: win_height
@@ -53,19 +61,18 @@ fn main() {
 			stretch: true
 			margin: ui.MarginConfig{8, 8, 8, 8}
 			spacing: 0
-		}, [
-			ui.button({
+		}, [	ui.button({
 				width: 80
 				height: 30
 				text: "start"
 				onclick:start_stop
 				}),
 			ui.button({
-				width: 110
+				width:110
 				height: 30
 				text: "$col columns"
 				onclick:click_column
-				})
+				}),
 			ui.button({
 				width: 110
 				height: 30
@@ -83,6 +90,7 @@ fn main() {
 	])
 	app.window = window
 	go app.run()
+	go app.handle_size(if sapp.dpi_scale()==0.0 {1.0} else {sapp.dpi_scale()})
 	ui.run(window)
 }
 fn new_map (mut app &App, mut btn &ui.Button) {
@@ -109,6 +117,26 @@ fn start_stop(mut app App, mut btn &ui.Button) {
 		}
 }
 
+fn (mut app App) handle_size(scale f32) {
+	mut w, mut h, mut uh, mut uw, mut hs, mut ws:=0, 0, 0, 0, 0, 0
+	//app.btn_padding_right=300
+	for {
+		w = int(sapp.width() / scale)
+		h = int(sapp.height() / scale)
+		
+		uh = (h - margin_top_to_grid - 2 * grid_padding - (app.map.height-1) * padding)
+		uw = (w - 2 * grid_padding - (app.map.width) * padding)
+		
+		hs = uh / app.map.height
+		ws = uw / app.map.width
+	
+		app.size = if hs > ws {ws} else {hs}
+		
+		app.margin_left = (uw - app.size * app.map.width) / 2
+		app.margin_top = (uh - app.size * app.map.height) / 2
+	}
+}
+
 fn (mut app App) run() {
 	for {
 		if app.start {
@@ -120,14 +148,13 @@ fn (mut app App) run() {
 
 fn draw_c(gg &gg.Context, mut app &App) {
 	
-	
 	// draw background color of grid
-	gg.draw_rect(0, margin_top_to_grid, (size+padding)*app.map.width-padding+2*grid_padding, (size+padding)*app.map.height-padding+2*grid_padding, bg_grid)
+	gg.draw_rect(app.margin_left, margin_top_to_grid+app.margin_top, (app.size+padding)*app.map.width-padding+2*grid_padding, (app.size+padding)*app.map.height-padding+2*grid_padding, bg_grid)
 	
 	//draw grid
 	for w in 0..app.map.width {
 		for h in 0..app.map.height {
-			gg.draw_rect((padding+size)*w+grid_padding, (padding+size)*h+margin_top_to_grid+grid_padding, size, size, if app.map.pattern[w][h] {life} else {dead} )
+			gg.draw_rect((padding+app.size)*w+grid_padding+app.margin_left, (padding+app.size)*h+margin_top_to_grid+grid_padding+app.margin_top, app.size, app.size, if app.map.pattern[w][h] {life} else {dead} )
 		}
 	}
 }
